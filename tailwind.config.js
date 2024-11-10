b405dedfb76bf13bd5b2bfdb3217049e988cafae
Vee Jay <veejaymejosomisol@gmail.com>
2024-11-10 14:57:29 +0800

    Add intial files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const { nextui } = require("@nextui-org/react");
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.jsx",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    darkMode: "class",
+    plugins: [nextui()],
+};
